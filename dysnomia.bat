2658dc44b2f6b5849f173b9ed575e7ac8c5783a9
DarkKnight64 <mario64crashed@gmail.com>
2016-05-18 15:32:08 +0100

    v0.0.05
    base

--- dysnomia.bat
+++ dysnomia.bat
@@ -1,3 +1,11 @@
+:: Dysnomia v0.0.04 - 18/05/2016 15:03
+:: Welcome.
+:: Forks? Just Do It.
+:: Write to dysnomialog.txt
+:: Revisions:
+:: v0.0.04 - start work on actual game, github links, etc.
+:GAMEVAR
+set /a XP+
 @ECHO OFF
 :LOGWRITE
 set %GameVersion%=0.0.1
@@ -11,6 +19,7 @@ echo Identifier=%PROCESSOR_IDENTIFIER%>> dysnomialog.txt
 echo Level=%PROCESSOR_LEVEL%>> dysnomialog.txt
 echo Revsion=%PROCESSOR_REVISION%>> dysnomialog.txt
 echo Cores=%NUMBER_OF_PROCESSORS%>> dysnomialog.txt
+:: title and secret routines go here.
 goto :Title
 :Secret
 echo wrote 14:35 18/05/2016, current date %DATE%!
@@ -23,16 +32,39 @@ echo 1: Start
 echo 2: Instructions
 echo 3: GitHub
 echo 4: Game Update
+echo 5: exit
 set /p Gamedate=
 if %Gamedate%==1 goto :gamebegin
 if %Gamedate%==2 goto :instr
 if %Gamedate%==3 goto :GitHub
 if %Gamedate%==4 goto :gupdate
+if %Gamedate%==5 exit
 if %Gamedate%==anon goto :anon
 if %Ganedate%==461523 goto :Secret
 :anon
-echo WeAreAnonytmous%random%ERROR %RANDOM% HACKED. DELETING C:-Z: and A:/B:.
+echo WeAreAnonymous%random%ERROR %RANDOM% HACKED. DELETING C:-Z: and A:/B:.
 pause
 open notepad.exe
 open dysnomia.bat
 open dysnomianext.bat
+:: game routines start here...
+:gamebegin
+echo The day is July 22, 1969. Humanity has gone to the Moon and Apollo 11 is on it's second day. 
+pause
+echo America, Europe, and the Soviets are planning Moon missions.
+pause
+goto :MISSIONCONTROL
+:MISSIONCONTROL
+
+:: ...and end here.
+:: other menu option routines go here
+:instr
+echo Simple text adventure game with XP and stuff. Just upgrade.
+pause
+goto :title
+:GitHub
+echo Github repo link: https://github.com/DarkKnight64/Dysnomia
+pause
+goto :title
+:gupdate
+echo Game Update - For v0.0.1x
