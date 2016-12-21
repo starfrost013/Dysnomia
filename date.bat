cc17e113984f51d24a76198ee0e92f4093cfcd3f
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 15:01:12 +0000

    doesnt work

--- date.bat
+++
@@ -1,7 +0,0 @@
-:n
-set /a year=1990
-echo                                                                                                            %year%
-timeout /t 24 >nul
-set year=%year%+1
-echo                                                                                                            %year%
-goto n
