0c4656c6691422546aa11c9819f9e1d89e8e24ae
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 17:43:02 +0000

    StarID v0.5 major update

--- StarID/0000.bat
+++ StarID/0000.bat
@@ -7,7 +7,14 @@ set StarCustomDesc=OMG TEST :: since there may be many stars, this might only be
 set StarLife=1000 :: life in years. Yes, 0.5 will have a date system.
 set StarSizeKM=250000 :: star size in km
 set DeathDistance=42500 :: if one ship, this is the distance that it will reach before breaking apart or burning or whatever.
-
+set HillSphere=19000000 :: there will be a planet and star creator. The maximum orbit for planets.
+set RocheLimit=900000 :: minimum distance for planets or break apart.
+set FlareStar=true :: if it is a flare star
+set CoronaSize=65000 :: corona size. Slightly higher than deathdistance. Heats ship.
+set Luminosity=0.14 :: msol
+set StarLifeStage=main :: life stage. gas, proto, main, red giant, white dwarf, black dwarf.
+set Habitable=0.09 :: Sets habitable lifespan
+set Age=50 :: star age
 :: StarID and PlanetID notes
 :: at the moment they are .bat, but they may become .dysnomiaid files.
-:: StarID Format v0.1
+:: StarID Format v0.5
