714086052dca688eb372f7995f54de743e76ac6c
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 19:21:14 +0000

    Alpha Cen A

---
+++ StarID/0020.bat
@@ -0,0 +1,22 @@
+:: StarID for stars, lol.
+set StarName=Alpha Centuari A :: name
+set SpectralType=G2 :: M9 LOL. anyway, spectral type of the star. Can be anywhere from Y9 (small brown dwarf) to O0 (raging hyper star), or WR (insane raging hyper star about to die), or Wx (dead white dwarf)
+set CanSupernova=false :: no supernovas 4 u
+set StarDesc=A moderately-sized yellow-white star.
+set StarCustomDesc=The second closest star to the Sun. :: since there may be many stars, this might only be for certain ones.
+set StarLife=1000 :: life in years. Yes, 0.5 will have a date system.
+set StarSizeKM=250000 :: star size in km
+set DeathDistance=1850000 :: if one ship, this is the distance that it will reach before breaking apart or burning or whatever.
+set HillSphere=90000000000 :: there will be a planet and star creator. The maximum orbit for planets.
+set RocheLimit=3500000 :: minimum distance for planets or break apart.
+set FlareStar=false :: if it is a flare star
+set CoronaSize=2000000 :: corona size. Slightly higher than deathdistance. Heats ship.
+set Luminosity=1.51 :: msol
+set StarLifeStage=main :: life stage. gas, proto, main, red giant, white dwarf, black dwarf.
+set Habitable=0.79 :: Sets habitable lifespan
+set Age=23000 :: star age
+set IsBinary=true :: is binary
+set BinaryOrbitalDistance=dominant :: binary orbital distance.
+:: StarID and PlanetID notes
+:: at the moment they are .bat, but they may become .dysnomiaid files.
+:: StarID Format v0.6
