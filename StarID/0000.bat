e4a1a30c71ae8eadf409af6432005827a79ff67e
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 13:04:56 +0000

    StarID v0.1

---
+++ StarID/0000.bat
@@ -0,0 +1,13 @@
+:: StarID for stars, lol.
+set StarName=teststar :: name
+set SpectralType=M9 :: M9 LOL. anyway, spectral type of the star. Can be anywhere from Y9 (small brown dwarf) to O0 (raging hyper star), or WR (insane raging hyper star about to die), or Wx (dead white dwarf)
+set CanSupernova=false :: no supernovas 4 u
+set StarDesc=Small red dwarf.
+set StarCustomDesc=OMG TEST :: since there may be many stars, this might only be for certain ones.
+set StarLife=1000 :: life in years. Yes, 0.5 will have a date system.
+set StarSizeKM=250000 :: star size in km
+set DeathDistance=42500 :: if one ship, this is the distance that it will reach before breaking apart or burning or whatever.
+
+:: StarID and PlanetID notes
+:: at the moment they are .bat, but they may become .dysnomiaid files.
+:: StarID Format v0.1
