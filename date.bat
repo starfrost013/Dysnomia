a0dafd48776c13f79097dd5179a51ae6802a73db
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 14:46:49 +0000

    ts

--- date.bat
+++ date.bat
@@ -4,3 +4,4 @@ echo                                                                            
 timeout /t 24 >nul
 set year=%year%+1
 echo                                                                                                            %year%
+goto n
