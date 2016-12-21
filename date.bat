4ad82664fbe710d20df5123a0866de79f84e965e
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 14:42:40 +0000

    bc

--- date.bat
+++ date.bat
@@ -1,8 +1,6 @@
-DATE=01-01-1990
-
-for i in {0..2147483647}
-timeout /t 1 >nul
-do
-   NEXT_DATE=$(date +%m-%d-%Y -d "$DATE + $i day")
-   echo                                             "$NEXT_DATE"
-done
+:n
+set year=1990
+echo                                                                                                            %year%
+timeout /t 24 >nul
+set year=%year%+1
+echo                                                                                                            %year%
