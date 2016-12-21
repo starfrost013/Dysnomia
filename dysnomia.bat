883d39a90b50802540b1dd156f9c01ea903a2f9b
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 14:01:17 +0000

    remove log writing code for seperation into seperate file
    done

--- dysnomia.bat
+++ dysnomia.bat
@@ -41,20 +41,6 @@ set burnpercent=0
 set gameversion=0.5
 @ECHO OFF
 title Dysnomia [v0.5]
-:logwrite
-echo Dysnomia
-echo Log Date: %date%
-echo Log Time: %date%
-echo Game Version=%gameversion% > dysnomialog.txt
-echo Windows Version=%OS%>> dysnomialog.txt
-echo Computer Name=%COMPUTERNAME%>> dysnomialog.txt
-echo Win Root=%SystemRoot%>> dysnomialog.txt
-echo Processor Information:>> dysnomialog.txt
-echo Architecture=%PROCESSOR_ARCHITECTURE%>> dysnomialog.txt
-echo Identifier=%PROCESSOR_IDENTIFIER%>> dysnomialog.txt
-echo Level=%PROCESSOR_LEVEL%>> dysnomialog.txt
-echo Revision=%PROCESSOR_REVISION%>> dysnomialog.txt
-echo Cores=%NUMBER_OF_PROCESSORS%>> dysnomialog.txt
 :: title and secret routines go here.
 goto :Title
 :Secret
