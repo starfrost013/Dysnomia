0338a4a82db9729de3ef65331cc02a35062718f4
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 14:16:32 +0000

    tweak
    p0-p

--- date.bat
+++ date.bat
@@ -1,3 +1,6 @@
+set %day%==1
+set %month%==1
+set %year%==1
 :main 
 echo                                                                                                  Date: %day% / %month% / %year%
 timeout /t 6 >nul
