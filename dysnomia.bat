86dfbed7189cc22e149e628d477b32da32f1c929
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 18:08:32 +0000

    rewrites, fixes.
    left/right=y
    up/down=x

--- dysnomia.bat
+++ dysnomia.bat
@@ -281,26 +281,23 @@ if %explorechoice%==4 goto :y2
 :y
 echo How many mKM (1 million KM = 1 mKM)?
 set /p mnx2=
-for %%A IN (%mnx2%) DO set %x%=%x%-1
+for %%A IN (%mnx2%) DO set %y%=%y%+1
 pause
 goto explore
 :y2
 echo How many mKM (1 million KM = 1 mKM)?
-set /p mnx2=
-for %%A IN (%mnx2%) DO set %x%=%x%+1
+set /p mnx3=
+for %%A IN (%mnx3%) DO set %y%=%y%-1
+goto explore
 :x
 echo How many mKM (1 million KM = 1 mKM)?
-set /p mnx2=
-for %%A IN (%mnx2%) DO set %y%=%y%+1
-:y
+set /p mnx4=
+for %%A IN (%mnx4%) DO set %x%=%x%+1
+goto explore
+:x2
 echo How many mKM (1 million KM = 1 mKM)?
-set /p mnx2=
-for %%A IN (%mnx2%) DO set %x%=%x%-1
-pause
-goto explore
-pause
-goto explore
-pause
+set /p mnx5=
+for %%A IN (%mnx5%) DO set %x%=%x%-1
 goto explore
 :mars
 call mars.bat
