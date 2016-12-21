8b0c9f7f7defc307d19fdc18d1a388f4d31f8daa
DarkKnight64 <mario64crashed@gmail.com>
2016-12-21 19:14:03 +0000

    Sun
    yeah

---
+++ StarID/0010.bat
@@ -0,0 +1,24 @@
+:: StarID for stars, lol.
+:: first 3 numbers: star ID
+:: 4th: If binary, Binary StarID
+set StarName=Sol :: name
+set SpectralType=G2 :: M9 LOL. anyway, spectral type of the star. Can be anywhere from Y9 (small brown dwarf) to O0 (raging hyper star), or WR (insane raging hyper star about to die), or Wx (dead white dwarf)
+set CanSupernova=false :: no supernovas 4 u
+set StarDesc=Moderately sized yellow-white star.
+set StarCustomDesc=OMG TEST :: since there may be many stars, this might only be for certain ones.
+set StarLife=50000 :: life in years. Yes, 0.5 will have a date system.
+set StarSizeKM=795000 :: star size in km
+set DeathDistance=1500000 :: if one ship, this is the distance that it will reach before breaking apart or burning or whatever.
+set HillSphere=78500000000 :: there will be a planet and star creator. The maximum orbit for planets.
+set RocheLimit=5600000 :: minimum distance for planets or break apart.
+set FlareStar=false :: if it is a flare star
+set CoronaSize=1800000 :: corona size. Slightly higher than deathdistance. Heats ship.
+set Luminosity=1.00 :: msol
+set StarLifeStage=main :: life stage. gas, proto, main, red giant, white dwarf, black dwarf.
+set Habitable=0.09 :: Sets habitable lifespan
+set Age=23000 :: star age
+set IsBinary=false :: is binary
+set BinaryOrbitalDistance=none :: binary orbital distance.
+:: StarID and PlanetID notes
+:: at the moment they are .bat, but they may become .dysnomiaid files.
+:: StarID Format v0.6
