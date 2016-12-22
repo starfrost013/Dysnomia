407d18817e50f9f0e55acb2e0ec8deffe79b8a9a
DarkKnight64 <mario64crashed@gmail.com>
2016-12-22 15:19:22 +0000

    prOxImA

---
+++ StarID/0040.bat
@@ -0,0 +1,22 @@
+:: StarID for stars, lol.
+set StarName=Proxima Centuari :: name
+set SpectralType=M6 :: M9 LOL. anyway, spectral type of the star. Can be anywhere from Y9 (small brown dwarf) to O0 (raging hyper star), or WR (insane raging hyper star about to die), or Wx (dead white dwarf)
+set CanSupernova=false :: no supernovas 4 u
+set StarDesc=Small red dwarf.
+set StarCustomDesc=The closest star to Earth. Presumably, you arrived here after warping out of the Kuiper belt. Has a nice planet. :: since there may be many stars, this might only be for certain ones.
+set StarLife=750000000 :: life in years. Yes, 0.5 will have a date system.
+set StarSizeKM=235000 :: star size in km
+set DeathDistance=315000 :: if one ship, this is the distance that it will reach before breaking apart or burning or whatever.
+set HillSphere=1450000000 :: there will be a planet and star creator. The maximum orbit for planets.
+set RocheLimit=675000 :: minimum distance for planets or break apart.
+set FlareStar=true :: if it is a flare star
+set CoronaSize=65000 :: corona size. Slightly higher than deathdistance. Heats ship.
+set Luminosity=0.0017 :: msol
+set StarLifeStage=main :: life stage. gas, proto, main, red giant, white dwarf, black dwarf.
+set Habitable=0.56 :: Sets habitable lifespan
+set Age=4000 :: star age
+set IsBinary=true :: is binary
+set BinaryOrbitalDistance=6773 :: binary orbital distance. AU.
+:: StarID and PlanetID notes
+:: at the moment they are .bat, but they may become .dysnomiaid files.
+:: StarID Format v0.6
