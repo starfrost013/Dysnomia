eb46a331694a0ff7f64b6e92f49cf7c5b520dea2
DarkKnight64 <mario64crashed@gmail.com>
2017-09-09 17:19:17 +0100

    Dysnomia Final Update Part 4

--- logwrite.bat
+++ logwrite.bat
@@ -1,8 +1,8 @@
 :logwrite
-echo Dysnomia
-echo Log Date: %date%
-echo Log Time: %date%
-echo Game Version=%gameversion% > dysnomialog.txt
+echo Dysnomia version 0.5.91 Final Update was initalized at %date%%time% > dysnomialog.txt
+echo Log Date: %date% >> dysnomialog.txt
+echo Log Time: %date% >> dysnomialog.txt
+echo Game Version=%gameversion% >> dysnomialog.txt
 echo Windows Version=%OS%>> dysnomialog.txt
 echo Computer Name=%COMPUTERNAME%>> dysnomialog.txt
 echo Win Root=%SystemRoot%>> dysnomialog.txt
@@ -12,3 +12,4 @@ echo Identifier=%PROCESSOR_IDENTIFIER%>> dysnomialog.txt
 echo Level=%PROCESSOR_LEVEL%>> dysnomialog.txt
 echo Revision=%PROCESSOR_REVISION%>> dysnomialog.txt
 echo Cores=%NUMBER_OF_PROCESSORS%>> dysnomialog.txt
+call dysnomia.bat
